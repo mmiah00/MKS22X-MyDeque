10061157e66da404e0cedaa4731219beb4b686fb
mmiah00 <mmiah00@stuty.edu>
2019-03-25 14:48:48 -0400

    writing

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,15 @@
 public class Calculator{
+  private static String[] operators = {"*", "/", "+", "-", "%"};
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
-      return 1.0;
+      String[] nums = s.split (" ");
+      MyDeque <String> a = new MyDeque <> (nums.length);
+      for (int x = 0; x < nums.length; x ++) {
+        a.addLast (nums[x]);
+      }
+
+      return 1.01;
     }
 }
